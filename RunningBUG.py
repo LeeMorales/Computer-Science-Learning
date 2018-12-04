478d69a80a9057255f5ad73f87e06930d0fc3281
MarkLee <38075314+MarkHooland@users.noreply.github.com>
2018-12-04 20:44:37 +0800

    still can't fix

--- RunningBUG.py
+++ RunningBUG.py
@@ -1,6 +1,21 @@
 import re
 import urllib
+import sys
 
+if sys.version_info[0] == 3:
+    from urllib.request import urlopen
+else:
+    # Not Python 3 - today, it is most likely to be Python 2
+    # But note that this might need an update when Python 4
+    # might be around one day
+    from urllib import urlopen
+
+
+# Your code where you can use urlopen
+with urlopen("http://www.python.org") as url:
+    s = url.read()
+
+print(s)
 def getHtml(url):
     page = urllib.urlopen(url)
     html = page.read()
