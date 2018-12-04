381de22a459c6e1336991cb7f7e9d631adedd883
MarkLee <38075314+MarkHooland@users.noreply.github.com>
2018-12-04 12:29:06 +0800

    USERS ID Identification

--- PythonLearning.py
+++ PythonLearning.py
@@ -31,7 +31,7 @@ s = (x1+x100)*100/2
 print (s)
 s = 'Python was started in 1989 by "Guido".Python is free and easy to learn.'
 print (s)
-
+"""
 name = input('Enter file:')
 handle = open(name, 'r')
 counts = dict()
@@ -121,3 +121,18 @@ if (W not in list):
     print("not in the list")
 else:
     print("W is in the list and the W is ", W)
+"""
+#is	        is 是判断两个标识符是不是引用自一个对象	x is y, 类似 id(x) == id(y) , 如果引用的是同一个对象则返回 True，否则返回 False
+#is not	    is not 是判断两个标识符是不是引用自不同对象	x is not y ， 类似 id(a) != id(b)。如果引用的不是同一个对象则返回结果 True，否则返回 False。
+#Examples:
+e = int(input("Please input the e: "))
+r = int(input("Please input the r: "))
+if (id(e) == id(r)):
+    print("E is R")
+else:
+    print("E is not R", "And The E is", e,",","The R is", r)
+#交换
+if (id(e) != id(r)):
+    print("E is not the R")
+else:
+    print("E is the R")
