3b1e05c7570bf51a21748b80b6ddc3993305893e
Lee <38075314+MarkHooland@users.noreply.github.com>
2018-12-10 12:26:37 +0800

    生成器

--- PythonLearning.py
+++ PythonLearning.py
@@ -217,9 +217,23 @@ if isinstance(myfloat, float) and myfloat == 10.0:
 if isinstance(myint, int) and myint == 20:
     print("Integer: %d" % myint)
 
-########################################################################################################################################3#
-print("迭代器")
-list = [int(input("Please input the list: "))]
-a = iter(list)
-for x in a:
-    print (x)
+##########################################################################################################################################
+print("生成器")
+ 
+import sys
+ 
+def fibonacci(n): # 生成器函数 - 斐波那契
+    a, b, counter = 0, 1, 0
+    while True:
+        if (counter > n): 
+            return
+        yield a
+        a, b = b, a + b
+        counter += 1
+f = fibonacci(10) # f 是一个迭代器，由生成器返回生成
+ 
+while True:
+    try:
+        print (next(f), end=" ")
+    except StopIteration:
+        sys.exit()
