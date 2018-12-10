7f849d94314f72db9ebd9f16d8632a2c3cb23409
Lee <38075314+MarkHooland@users.noreply.github.com>
2018-12-10 12:18:24 +0800

    sys

--- 迭代器.py
+++ 迭代器.py
@@ -1,4 +1,10 @@
-list = [input("Please input the list: ")]
-a = iter(list)
-for x in a:
-    print (x)
+import sys         # 引入 sys 模块
+ 
+list=[1,2,3,4]
+it = iter(list)    # 创建迭代器对象
+ 
+while True:
+    try:
+        print (next(it))
+    except StopIteration:
+        sys.exit()
