8acc4a1dea7bbd00fdb7389c2374083f5be53af0
Lee <38075314+MarkHooland@users.noreply.github.com>
2018-12-17 16:47:17 +0800

    搞懂了

--- 二分查找.py
+++ 二分查找.py
@@ -1,28 +1,28 @@
-def binary_search(list, item):
-      # low and high keep track of which part of the list you'll search in.
+# 定义binary_search,但我也不知道为什么要在这定义......
+def binary_search(list, item):  # low和high跟踪你将搜索的列表的哪个部分
   low = 0
   high = len(list) - 1
 
-  # While you haven't narrowed it down to one element ...
+#只要范围没有缩小到只包含一个元素,就检查中间元素
   while low <= high:
-    # ... check the middle element
+    #查找中间数
     mid = (low + high) // 2
     guess = list[mid]
-    # Found the item.
+    # 找到了元素
     if guess == item:
       return mid
-    # The guess was too high.
+    # 猜的数字大了
     if guess > item:
       high = mid - 1
-    # The guess was too low.
+    # 猜的数字小了
     else:
       low = mid + 1
 
-  # Item doesn't exist
+  # 没有指定元素
   return None
 
 my_list = [1, 3, 5, 7, 9]
 print(binary_search(my_list, 3)) # => 1
 
-# 'None' means nil in Python. We use to indicate that the item wasn't found.
+# 在python中,None表示空,意味着没有找到指定的元素
 print(binary_search(my_list, -1)) # => None
