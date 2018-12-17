aad4d786c7e068f2b43b40a4cff29a457264b5c4
Lee <38075314+MarkHooland@users.noreply.github.com>
2018-12-17 16:32:16 +0800

    更多问题

--- 二分查找.py
+++ 二分查找.py
@@ -1,6 +1,5 @@
 low = 0
 high = len(list) - 1
-def_list = [1, 3, 5, 7, 9]
 mid = (low + high)
 guess = list[mid]
 if guess < item:
@@ -8,17 +7,15 @@ if guess < item:
 def binary_search(list, item):
   low = 0
   high = len(list)-1
-
 while low <= high:
   mid = (low + high)
   guess = list[mid]
   if guess == item:
-      return mid
+      return mid 
   if guess > item:
         high = mid - 1
   else:
       low = mid + 1
 return None
-
 print (binary_search(list, 3))
 print (binary_search(list, -1))
