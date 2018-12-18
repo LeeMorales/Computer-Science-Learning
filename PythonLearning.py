888dd91a75fd558c301b9caf0b75e9b6670e8d6d
Lee <38075314+MarkHooland@users.noreply.github.com>
2018-12-18 18:27:04 +0800

    添加选择排序

--- PythonLearning.py
+++ PythonLearning.py
@@ -266,4 +266,25 @@ print(binary_search(my_list, 3)) # => 1
 
 # 在python中,None表示空,意味着没有找到指定的元素
 print(binary_search(my_list, -1)) # => None
-
+#################################################################################################################################
+print("选择排序")
+# 在数组中找到最小值
+def findSmallest(arr):
+  # 存储最小值
+  smallest = arr[0]
+  # 存储最小元素的索引
+  smallest_index = 0
+  for i in range(1, len(arr)):
+    if arr[i] < smallest:
+      smallest_index = i
+      smallest = arr[i]      
+  return smallest_index
+# 对数组进行排序
+def selectionSort(arr):
+  newArr = []
+  for i in range(len(arr)):
+      # 找出数组中最小的元素，并将其加入到新的数组中
+      smallest = findSmallest(arr)
+      newArr.append(arr.pop(smallest))
+  return newArr
+print(selectionSort([5, 3, 6, 2, 10]))
