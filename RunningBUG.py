9afd684d587581b8ac0cb9c780952776890658db
Lee <38075314+MarkHooland@users.noreply.github.com>
2018-12-22 11:01:12 +0800

    简单爬虫

--- RunningBUG.py
+++ RunningBUG.py
@@ -1,31 +1,3 @@
-import re
 import urllib
-import sys
-
-if sys.version_info[0] == 3:
-    from urllib.request import urlopen
-else:
-    # Not Python 3 - today, it is most likely to be Python 2
-    # But note that this might need an update when Python 4
-    # might be around one day
-    from urllib import urlopen
-
-
-# Your code where you can use urlopen
-with urlopen("http://www.python.org") as url:
-    s = url.read()
-
-print(s)
-def getHtml(url):
-    page = urllib.urlopen(url)
-    html = page.read()
-    return html
-
-def getImg(html):
-    reg = r'src="(.+?\.jpg)" pic_ext'
-    imgre = re.compile(reg)
-    imglist = re.findall(imgre,html)
-    return imglist      
-   
-html = getHtml("https://blog.csdn.net/jiangwei0512/article/details/52079031");
-print (getImg(html))
+response = urllib.request.urlopen("http://www.zhihu.com")
+print (response.read())
