d9fa46fad18d876599bc8728daa100f6b53ed6ae
Lee <38075314+MarkHooland@users.noreply.github.com>
2018-12-27 18:35:25 +0800

    加入D&C与快速排序

--- D&C与快速排序.py
+++ D&C与快速排序.py
@@ -3,4 +3,23 @@ def sum(arr):
    for x in arr:
     total += x
    return total
-print (sum([1, 2, 3, 4])) 
+print (sum([1, 2, 3, 4]))
+####################################################################################
+# #给定一个list
+def sum(list):
+#调用sum函数
+      if list == []:
+#基线条件
+          return 0
+#递归条件
+          return list[0] + sum(list[1:])
+#####################################################################################
+def quicksort(array):
+      if len(array) < 2:
+       return array
+      else:
+             pivot = array[0]
+             less = [i for i in array[1:] if i <= pivot]
+             greater = [i for i in array[1:] if i > pivot]
+             return quicksort(less) + [pivot] + quicksort(greater)
+print(quicksort([10, 5, 2, 3]))
