fc0dbc4e3b2891ebd86dde5631ef321d339e4a45
Lee <38075314+MarkHooland@users.noreply.github.com>
2018-12-27 18:29:28 +0800

    反正就是没有输出

--- 递归.py
+++ 递归.py
@@ -3,16 +3,3 @@ def greet(name):
     greet2(name)
     print ("getting ready to say goodbye")
     bye()
-def greet2(name):
-    print ("How are you?")
-def bye():
-    print ("OK,Bye!")
-def greet(name):
-    print ("hello")
-def greet2(name):
-    print("How are you?")
-def f(x):
-    if x == 1:
-        return 1
-    else:
-        return x * f(x-1)
