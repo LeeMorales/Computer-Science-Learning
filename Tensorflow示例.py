4cb98da41f4c4ba10d23c416596784baad1ab4eb
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-01-06 10:07:02 +0800

    慢慢修

--- Tensorflow示例.py
+++
@@ -1,8 +0,0 @@
-import pytorch as 
-a = .constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-b = .constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-c = .matmul(a, b)
-
-sess = .Session(config=tf.ConfigProto(log_device_placement=True))
-
-print (sess.run(c))
