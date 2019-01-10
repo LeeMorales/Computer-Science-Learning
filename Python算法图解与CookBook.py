1b90ade68725d53d32c387fc2f72da3655b803b9
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-01-10 18:27:21 +0800

    并行分布式算法有点死循环的问题，所以我单开了一个分支

--- Python算法图解与CookBook.py
+++ Python算法图解与CookBook.py
@@ -249,24 +249,7 @@ root.PrintTree()
 ###############################################################################################################################################
 
 ##############################################################################################################################################
-print("并行，分布式算法")
-import multiprocessing
-
-def f(x):
-    return x * x
-
-cores = multiprocessing.cpu_count()
-pool = multiprocessing.Pool(processes=cores)
-xs = range(5)
-
-# method 1: map
-print (pool.map(f, xs))  # prints [0, 1, 4, 9, 16]
-
-# method 2: imap
-for y in pool.imap(f, xs):
-    print(y)           
-for y in pool.imap_unordered(f, xs):
-    print(y)           # may be in any order
+          # may be in any order
 ####################################################################################################################################################################
 print("数据结构与算法1")
 def drop_first_last(grades):
