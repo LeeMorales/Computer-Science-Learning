ca79dad545345d0f7f6656d04ea80415e1a18b89
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-01-12 13:42:17 +0800

    Python Learning更新到字符串与文本5

--- PythonLearning.py
+++ PythonLearning.py
@@ -772,4 +772,151 @@ def matchcase(word):
             return word.capitalize()
         else:
             return word
-        return replace  
+        return replace 
+#######################################################################################################################
+print("字符串与文本4")
+import sys
+
+class Info:
+    def __init__(self, name, n):
+        self.name = name
+        self.n = n
+
+
+class SafeSub(dict):
+    """防止key找不到"""
+    def __missing__(self, key):
+        return '{' + key + '}'
+
+
+def sub(text):
+    return text.format_map(SafeSub(sys._getframe(1).f_locals))
+
+def var_str():
+    s = '{name} has {n} messages.'
+    print(s.format(name='Guido', n=37))
+
+    # vars()和format_map
+    a = Info('Guido', 37)
+    print(s.format_map(vars(a)))
+
+    name = 'Lisi'
+    print(s.format_map(SafeSub(vars())))
+
+    name = 'Guido'
+    n = 37
+    print(sub('Hello {name}'))
+    print(sub('You have {n} messages.'))
+    print(sub('Your favorite color is {color}'))
+
+
+if __name__ == '__main__':
+    var_str()
+
+###################################################################################################################
+print("字符串与文本5")
+import textwrap
+import os
+
+
+def reformat_width():
+    s = "No man is an island, entire it self, Every man is a piece of the continent，a part of the main."
+
+    print(textwrap.fill(s, 70))
+    print('*' * 40)
+    print(textwrap.fill(s, 40))
+    print('*' * 40)
+    print(textwrap.fill(s, 40, initial_indent='    '))
+    print('*' * 40)
+    print(textwrap.fill(s, 40, subsequent_indent='    '))
+
+import html
+
+
+def html_xml():
+    s = 'Elements are written as "<tag>text</tag>".'
+    print(s)
+    print(html.escape(s))
+
+    # Disable escaping of quotes
+    print(html.escape(s, quote=False))
+
+    s = 'Spicy Jalapeño'
+    print(s.encode('ascii', errors='xmlcharrefreplace'))
+
+if __name__ == '__main__':
+    html_xml()
+
+import re
+from collections import namedtuple
+
+
+def tokenize_str():
+    text = 'foo = 23 + 42 * 10'
+    tokens = [('NAME', 'foo'), ('EQ', '='), ('NUM', '23'), ('PLUS', '+'),
+              ('NUM', '42'), ('TIMES', '*'), ('NUM', '10')]
+    NAME = r'(?P<NAME>[a-zA-Z_][a-zA-Z_0-9]*)'
+    NUM = r'(?P<NUM>\d+)'
+    PLUS = r'(?P<PLUS>\+)'
+    TIMES = r'(?P<TIMES>\*)'
+    EQ = r'(?P<EQ>=)'
+    WS = r'(?P<WS>\s+)'
+
+    master_pat = re.compile('|'.join([NAME, NUM, PLUS, TIMES, EQ, WS]))
+
+    scanner = master_pat.scanner('foo = 42')
+    a = scanner.match()
+    print(a)
+    print((a.lastgroup, a.group()))
+    a = scanner.match()
+    print(a)
+    print((a.lastgroup, a.group()))
+    a = scanner.match()
+    print(a)
+    print((a.lastgroup, a.group()))
+    a = scanner.match()
+    print(a)
+    print((a.lastgroup, a.group()))
+    a = scanner.match()
+    print(a)
+    print((a.lastgroup, a.group()))
+    a = scanner.match()
+    print(a)
+
+    # 实际生成器代码
+
+    # Example use
+    for tok in generate_tokens(master_pat, 'foo = 42'):
+        print(tok)
+    # Produces output
+    # Token(type='NAME', value='foo')
+    # Token(type='WS', value=' ')
+    # Token(type='EQ', value='=')
+    # Token(type='WS', value=' ')
+    # Token(type='NUM', value='42')
+    tokens = (tok for tok in generate_tokens(master_pat, text)
+              if tok.type != 'WS')
+    for tok in tokens:
+        print(tok)
+
+    print('*'*40)
+
+    LT = r'(?P<LT><)'
+    LE = r'(?P<LE><=)'
+    EQ = r'(?P<EQ>=)'
+    master_pat = re.compile('|'.join([LE, LT, EQ])) # Correct
+    # master_pat = re.compile('|'.join([LT, LE, EQ])) # Incorrect
+
+
+
+
+def generate_tokens(pat, text):
+    Token = namedtuple('Token', ['type', 'value'])
+    scanner = pat.scanner(text)
+    for m in iter(scanner.match, None):
+        yield Token(m.lastgroup, m.group())
+
+
+if __name__ == '__main__':
+    tokenize_str()     
+############################################################################################################################
