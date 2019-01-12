ee6fdbf6b4c323739033fd6eab471f08cf64bf01
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-01-12 11:00:07 +0800

    更新到字符串与文本4

--- Python算法图解与CookBook.py
+++ Python算法图解与CookBook.py
@@ -300,7 +300,7 @@ heapq.heapify(heap)
 print (heap)
 import heapq
 class priorityqueue:
-    def _init_(self):
+    def _init_(self):；m
       self.queue = []
       self._index = 0
 def push(self, item, priority, self_index):
@@ -409,10 +409,8 @@ def value():
 print("数据结构与算法6")
 prices = ("ACME:45.23", "AAPL: 612.78", "IBM: 205.55", "HPQ: 37.20", "FB: 10.75")
 def tech_names():
-#创建一个价格大于200的字典
-p1 = { key:value for key, value in prices.items() if value > 200}
-#创建一个技术类的股票
-p2 = { key:value for key, value in prices.items() if key in tech_names}
+    p1 = { key:value for key, value in prices.items() if value > 200}
+    p2 = { key:value for key, value in prices.items() if key in tech_names}
 
 from collections import namedtuple
 Stock = namedtuple('Stock', ['name', 'shares', 'price'])
@@ -468,4 +466,43 @@ def matchcase(word):
             return word.capitalize()
         else:
             return word
-        return replace  
+        return replace
+#######################################################################################################################
+print("字符串与文本4")
+import sys
+
+class Info:
+    def __init__(self, name, n):
+        self.name = name
+        self.n = n
+
+
+class SafeSub(dict):
+    """防止key找不到"""
+    def __missing__(self, key):
+        return '{' + key + '}'
+
+
+def sub(text):
+    return text.format_map(SafeSub(sys._getframe(1).f_locals))
+
+def var_str():
+    s = '{name} has {n} messages.'
+    print(s.format(name='Guido', n=37))
+
+    # vars()和format_map
+    a = Info('Guido', 37)
+    print(s.format_map(vars(a)))
+
+    name = 'Lisi'
+    print(s.format_map(SafeSub(vars())))
+
+    name = 'Guido'
+    n = 37
+    print(sub('Hello {name}'))
+    print(sub('You have {n} messages.'))
+    print(sub('Your favorite color is {color}'))
+
+
+if __name__ == '__main__':
+    var_str()
