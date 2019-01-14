f7a9808371cd59e6d32fa3879faa93f8cf4c9a40
Miles Morales <38075314+MarkHooland@users.noreply.github.com>
2019-01-14 12:39:42 +0800

    Add files via upload

---
+++ 选择排序.py
@@ -0,0 +1,22 @@
+# 在数组中找到最小值
+def findSmallest(arr):
+  # 存储最小值
+  smallest = arr[0]
+  # 存储最小元素的索引
+  smallest_index = 0
+  for i in range(1, len(arr)):
+    if arr[i] < smallest:
+      smallest_index = i
+      smallest = arr[i]      
+  return smallest_index
+
+# 对数组进行排序
+def selectionSort(arr):
+  newArr = []
+  for i in range(len(arr)):
+      # 找出数组中最小的元素，并将其加入到新的数组中
+      smallest = findSmallest(arr)
+      newArr.append(arr.pop(smallest))
+  return newArr
+
+print(selectionSort([5, 3, 6, 2, 10]))
