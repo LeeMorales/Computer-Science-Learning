25dac42bacbad7eb3f0edeb3a2f850f75dbe77f3
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-01-22 16:14:48 +0800

    完成！看看我能不能打包成EXE

--- Code_Learning/整蛊垃圾微信信息.py
+++ Code_Learning/整蛊垃圾微信信息.py
@@ -2,8 +2,8 @@ import  itchat
 import  time
 itchat.auto_login()
 
-user=itchat.search_friends(nickName="rong's")[0]
+user=itchat.search_friends(nickName="PureBlue_恒")[0]
 for i in range(10):
-    itchat.send_msg(msg="我错了",toUserName=user['UserName'])
+    itchat.send_msg(msg="You were being hacked",toUserName=user['UserName'])
     time.sleep(0)
 itchat.logout()
