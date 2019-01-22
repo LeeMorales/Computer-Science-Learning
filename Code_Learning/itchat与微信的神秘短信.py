ac0c0d9ba8900aa1b4fee76e881f3a99f1278a2c
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-01-22 15:57:20 +0800

    加入消息获取功能

--- Code_Learning/itchat与微信的神秘短信.py
+++ Code_Learning/itchat与微信的神秘短信.py
@@ -1,6 +1,23 @@
 import itchat
 itchat.login()
 def check_login(self, uuid=None):
+    def get_contact(self, update=False):
+        raise NotImplementedError()
+    def get_friends(self, update=False):
+        raise NotImplementedError()
+    def get_chatrooms(self, update=False, contactOnly=False):
+        raise NotImplementedError()
+    def get_maps(self, update=False):
+        raise NotADirectoryError()
+    from  itchat.storage.templates import ContactList
+    itchat.auto_login()
+    print(itchat.get_contact())
+    print(itchat.get_chatrooms())
+    print(itchat.get_friends())
+    print(itchat.get_mps())
+    print(type(itchat.get_contact()))
+    print(type(itchat.get_chatrooms()))
+    print(type(itchat.get_friends()))
+    print(type(itchat.get_mps()))    
     def logout(self):
         raise NotImplementedError()
-
