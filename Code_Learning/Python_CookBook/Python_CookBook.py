bad65218f0dc14953ed7164371d913745d7ff0b8
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-01-24 17:24:46 +0800

    改了些小bug

--- Code_Learning/Python_CookBook/Python_CookBook.py
+++ Code_Learning/Python_CookBook/Python_CookBook.py
@@ -1669,87 +1669,79 @@ def process_pipline():
     files = gen_opener(lognames)
     lines = gen_concatenate(files)
     pylines = gen_grep('(?i)python', lines)
-    for line in pylines:
-        print(line)
-
-lognames = gen_find('access-log*', 'www')
-    files = gen_opener(lognames)
-    lines = gen_concatenate(files)
-    pylines = gen_grep('(?i)python', lines)
     bytecolumn = (line.rsplit(None, 1)[1] for line in pylines)
     bytes = (int(x) for x in bytecolumn if x != '-')
     print('Total', sum(bytes))
+    if __name__ == '__main__':
+        process_pipline()
 
-
-if __name__ == '__main__':
-    process_pipline()
-
-from collections import Iterable
+    from collections import Iterable
 
 def flatten(items, ignore_types=(str, bytes)):
-    for x in items:
-        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
-            yield from flatten(x)
-        else:
-            yield x
+    def iterable():
+        for x in items:
+            if isinstance(x, iterable) and not isinstance(x, ignore_types):
+                yield from flatten(x)
+            else:
+                yield x
 
 
-def flatten_seq():
-    items = [1, 2, [3, 4, [5, 6], 7], 8]
-    # Produces 1 2 3 4 5 6 7 8
-    for x in flatten(items):
+    def flatten_seq():
+        items = [1, 2, [3, 4, [5, 6], 7], 8]
+        # Produces 1 2 3 4 5 6 7 8
+        for x in flatten(items):
+            print(x)
+        items = ['Dave', 'Paula', ['Thomas', 'Lewis']]
+            for x in flatten(items):
         print(x)
-    items = ['Dave', 'Paula', ['Thomas', 'Lewis']]
-    for x in flatten(items):
-        print(x)
 
-if __name__ == '__main__':
-    flatten_seq()
+    if __name__ == '__main__':
+        flatten_seq()
 
-import heapq
+    import heapq
 
 
-def merge_sorted():
-    a = [1, 4, 7, 10]
-    b = [2, 5, 6, 11]
-    for c in heapq.merge(a, b):
-        print(c)
+    def merge_sorted():
+        a = [1, 4, 7, 10]
+        b = [2, 5, 6, 11]
+        for c in heapq.merge(a, b):
+            print(c)
 
-    # 合并排序文件
-    with open('sorted_file_1', 'rt') as file1, \
-            open('sorted_file_2', 'rt') as file2, \
-            open('merged_file', 'wt') as outf:
+        # 合并排序文件
+        with open('sorted_file_1', 'rt') as file1, \
+                open('sorted_file_2', 'rt') as file2, \
+                open('merged_file', 'wt') as outf:
 
-        for line in heapq.merge(file1, file2):
+                for line in heapq.merge(file1, file2):
             outf.write(line)
 
 
-if __name__ == '__main__':
-    merge_sorted()
+    if __name__ == '__main__':
+        merge_sorted()
 
-import sys
+    import sys
 
-def process_data():
-    print(data)
-
-
-def reader(s, size):
-    while True:
-        data = s.recv(size)
-        if data == b'':
-            break
-            # process_data(data)
-def reader2(s, size):
-    for data in iter(lambda: s.recv(size), b''):
-        process_data(data)
-def iterate_while():
-    CHUNKSIZE = 8192
-    with open('/etc/passwd') as f:
-        for chunk in iter(lambda: f.read(10), ''):
-            n = sys.stdout.write(chunk)
+    def process_data():
+        print(data)
 
 
-if __name__ == '__main__':
-    iterate_while()
+    def reader(s, size):
+        while True:
+            data = s.recv(size)
+            if data == b'':
+                break
+                # process_data(data)
+    def reader2(s, size):
+        for data in iter(lambda: s.recv(size), b''):
+            process_data(data)
+    def iterate_while():
+        CHUNKSIZE = 8192
+        with open('/etc/passwd') as f:
+            for chunk in iter(lambda: f.read(10), ''):
+                n = sys.stdout.write(chunk)
+
+
+    if __name__ == '__main__':
+        iterate_while()
 ########################################################################################################################
 
