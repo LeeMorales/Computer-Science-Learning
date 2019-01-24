e51a0d8582aaebecd34fa6b9f9d8cab46af7b83d
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-01-24 21:29:26 +0800

    第一个求和就卡住了

---
+++ Code_Learning/LeetCode刷题.py
@@ -0,0 +1,28 @@
+print("Nums求总和为9")
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        list = [2, 7, 11, 15]
+        if (nums[0] + nums[1] = 9)
+           print(0, 1)
+        elif
+            (nums[0] + nums[2] = 9)
+                print(0, 2)
+        elif:
+            nums[0] + nums[3] = 9:
+                print(0, 3)
+        elif:
+            nums[1] + nums[2] = 9:
+                print(1, 2)
+        elif:
+            nums[1] + nums[3] = 9:
+                print(1, 3)
+        elif:
+            nums[2] + nums[3] = 9:
+                print(2, 3)
+        else:
+                print("None of them add to 9")
