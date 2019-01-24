57bc2be74c6f72cc6eb93d1f5618e6834e3fdb45
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-01-24 22:23:42 +0800

    Update LeetCode刷题.py

--- Code_Learning/LeetCode刷题.py
+++ Code_Learning/LeetCode刷题.py
@@ -7,22 +7,4 @@ class Solution:
         :rtype: List[int]
         """
         list = [2, 7, 11, 15]
-        if (nums[0] + nums[1] = 9)
-           print(0, 1)
-        elif
-            (nums[0] + nums[2] = 9)
-                print(0, 2)
-        elif:
-            nums[0] + nums[3] = 9:
-                print(0, 3)
-        elif:
-            nums[1] + nums[2] = 9:
-                print(1, 2)
-        elif:
-            nums[1] + nums[3] = 9:
-                print(1, 3)
-        elif:
-            nums[2] + nums[3] = 9:
-                print(2, 3)
-        else:
-                print("None of them add to 9")
+    if 
