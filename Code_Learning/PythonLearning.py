5475dae7268d579b1299c00a5073ddaf89a7b66d
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-01-26 12:30:57 +0800

    更新到了文件和I/O

--- Code_Learning/PythonLearning.py
+++ Code_Learning/PythonLearning.py
@@ -2282,3 +2282,42 @@ def flatten_seq():
     if __name__ == '__main__':
         iterate_while()
 ########################################################################################################################
+print("文件和I/O")
+def rw_text():
+    # Iterate over the lines of the file
+    with open('somefile.txt', 'rt') as f:
+        for line in f:
+            # process line
+            print(line)
+
+    # Write chunks of text data
+    with open('somefile.txt', 'wt') as f:
+        f.write('text1')
+        f.write('text2')
+
+if __name__ == '__main__':
+    rw_text()
+
+def print_tofile():
+    with open('d:/work/test.txt', 'wt') as f:
+        print('Hello World!', file=f)
+
+if __name__ == '__main__':
+    print_tofile()
+
+def print_sepend():
+    print('ACME', 50, 91.5)
+    print('ACME', 50, 91.5, sep=',')
+    print('ACME', 50, 91.5, sep=',', end='!!\n')
+    for i in range(5):
+        print(i)
+    for i in range(5):
+        print(i, end=' ')
+    print()
+
+    row = ['ACME', 50, 91.5]
+    print(*row, sep=',')
+
+if __name__ == '__main__':
+    print_sepend()
+############################################################################################################################
