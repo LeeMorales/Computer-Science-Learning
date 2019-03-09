76fba3c7e4f74137fef70739f7634452a25c11ca
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-03-09 18:22:10 +0800

    修了点报错

--- Code_Learning/Python_CookBook/函数.py
+++ Code_Learning/Python_CookBook/函数.py
@@ -1,5 +1,5 @@
 def avg(first, *rest):
-return (first + sum(rest)) / (1 + len(rest))
+    return (first + sum(rest)) / (1 + len(rest))
 import html
 def make_element(name, value, **attrs):
     keyvals = [' %s="%s"' % item for item in attrs.items()]
@@ -8,7 +8,7 @@ def make_element(name, value, **attrs):
                   name=name,
                   attrs=attr_str,
                   value=html.escape(value))
-return element
+    return element
 def anyargs(*args, **kwargs):
     print(args)
     print(kwargs)
@@ -17,13 +17,13 @@ def anyargs(*args, **kwargs):
     pass
 
 # recv(1024, True) # TypeError
-recv(1024, block=True)  # Ok
-def minimum(*values, clip=None):
-    m = min(values)
-    if clip is not None:
-        m = clip if clip > m else m
-    return m
+    recv(1024, block=True)  # Ok
+    def minimum(*values, clip=None):
+        m = min(values)
+        if clip is not None:
+            m = clip if clip > m else m
+        return m
 
 
-minimum(1, 5, 2, -5, 10)  # Returns -5
-minimum(1, 5, 2, -5, 10, clip=0)  # Returns 0
+    minimum(1, 5, 2, -5, 10)  # Returns -5
+    minimum(1, 5, 2, -5, 10, clip=0)  # Returns 0
