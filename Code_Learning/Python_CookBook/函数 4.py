568902ea7e92dcc7719df4691a78c32036081b57
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-03-09 18:08:53 +0800

    函数4的例子里面因为网页打不开而报错

--- Code_Learning/Python_CookBook/函数 4.py
+++ Code_Learning/Python_CookBook/函数 4.py
@@ -7,7 +7,7 @@ class UrlTemplate:
 
     def open(self, **kwargs):
         return urlopen(self.template.format_map(kwargs))
-
+"""
 # Example use. Download stock data from yahoo
 yahoo = UrlTemplate('http://finance.yahoo.com/d/quotes.csv?s={names}&f={fields}')
 for line in yahoo.open(names='IBM,AAPL,FB', fields='sl1c1v'):
@@ -28,3 +28,4 @@ for line in yahoo(names='IBM,AAPL,FB', fields='sl1c1v'):
 
     # Invoke the callback with the result
         callback(result)
+"""
