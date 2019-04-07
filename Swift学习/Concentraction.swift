08cc0d0b92c063b41b0adafa2aa4761f8d2df797
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-04-07 18:55:43 +0800

    更新多个按钮

--- Swift学习/Concentraction.swift
+++ Swift学习/Concentraction.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController 
 {
-    var flipCount: Int = 0 
+    var flipCount: Int = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+
+    @IBOutlet weak var flipCountLabel: UILabel!
+
+    @IBOutlet var cardButtons: [UIButton]!
 
     @IBAction func touchCard(_ sender: UIButton){
         flipCount += 1
-        flipCard(withEmoji:"🤣", on: sender)
+        
     }
-    @IBAction func touchSecondCard(_ sender: UIButton){
-        flipCount += 1
-        flipCard(withEmoji:"🤣", on: sender)
-    }
+    
     func flipCard(withEmoji emoji: String, on button: UIButton){
         if button.currentTitle == emoji {
             button.setTitle("", for: UIContrlState.normal)
