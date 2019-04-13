92acb3b86b109ad36c6fce51733cd92dcc5fd6b6
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-04-13 16:03:44 +0800

    更新输入点位置

--- Code_Learning/凯撒移位.py
+++ Code_Learning/凯撒移位.py
@@ -4,7 +4,7 @@ times=0
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 alphabet_upper=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 print("Encode or decode?")
-input E
+
 plain=input("Please input your plain text: ")
 value=input("Please input your key(included negatives): ")
   
