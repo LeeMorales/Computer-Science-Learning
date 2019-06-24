6b47d33cc9212264e5464a2356dac583fbfb6505
Lee Morales <grandtheftauto710@outlook.com>
2019-06-24 22:57:39 +0800

    消除报错

--- Python相关/算法图解/1.1 二分查找.py
+++ Python相关/算法图解/1.1 二分查找.py
@@ -1,17 +1,17 @@
 #二分查找
 ##思路：输入一列有序的元素列表，如果要查找的元素包含在列表中，二分查找返回其位置，否则返回null
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-low = 0
-high = len(list) - 1
-mid = (low + high) // 2
-guess = list(mid)
-
-if guess < item:
-    low = mid + 1
+#coding=utf-8
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 def binary_search(list, item):
     low = 0
     high = len(list)-1
+    low = 0
+    high = len(list) - 1
+    mid = (low + high) // 2
+    guess = list(mid)
 
+    if guess < item:
+        low = mid + 1
     while low <= high:
         mid = (low + high) / 2
         guess = list[mid]
@@ -23,8 +23,8 @@ def binary_search(list, item):
             low = mid + 1
         return None
 
-print (binary_search(my_list, 5))
-print (binary_search(my_list, 9))
+print (binary_search(list, 5))
+print (binary_search(list, 9))
 
 
 
