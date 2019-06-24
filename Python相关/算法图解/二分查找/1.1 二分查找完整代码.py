3486a0330969d167afa2821e5ee5f5e5a35cc8dd
Lee Morales <grandtheftauto710@outlook.com>
2019-06-24 23:40:49 +0800

    更新注释文件

--- Python相关/算法图解/二分查找/1.1 二分查找完整代码.py
+++ Python相关/算法图解/二分查找/1.1 二分查找完整代码.py
@@ -6,17 +6,28 @@ def binary_search(list, item):
 ##low和high用于跟踪要在其中的查找的列表部分
     low = 0
     high = len(list) - 1
-
+#----------------------------------
+##只要范围没有缩小到只包含一个元素就检查中间元素
     while low <= high:
         mid = (low + high) / 2
         guess = list[mid]
+#----------------------------------
+##找到了元素
         if guess == item:
             return mid
+#----------------------------------
+##数字大了
         if guess > item :
             high = mid - 1
+#----------------------------------
+##数字小了
         else:
             low = mid + 1
+#----------------------------------
+##没有指定元素
         return None
-
-print (binary_search(list, 5))
-print (binary_search(list, 9))       
+#----------------------------------
+##测试
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print (binary_search(my_list, 5))
+print (binary_search(my_list, 9))       
