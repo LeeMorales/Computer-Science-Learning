17c2ba6ab6c7870d4cfa3dd3f8500cdbb605b7d9
Lee Morales <grandtheftauto710@outlook.com>
2019-06-24 23:37:46 +0800

    更新注释

--- Python相关/算法图解/二分查找/1.1 二分查找完整代码.py
+++ Python相关/算法图解/二分查找/1.1 二分查找完整代码.py
@@ -1,3 +1,22 @@
+#二分查找
+##思路：输入一列有序的元素列表，如果要查找的元素包含在列表中，二分查找返回其位置，否则返回null
+#coding=utf-8
 def binary_search(list, item):
+#----------------------------------
+##low和high用于跟踪要在其中的查找的列表部分
     low = 0
     high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) / 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item :
+            high = mid - 1
+        else:
+            low = mid + 1
+        return None
+
+print (binary_search(list, 5))
+print (binary_search(list, 9))       
