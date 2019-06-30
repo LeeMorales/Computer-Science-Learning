7ecd38933a8b6b2ba99b6dbf860e43fa5a981c90
Lee Morales <grandtheftauto710@outlook.com>
2019-06-30 10:56:07 +0800

    更新选择排序算法

--- Python相关/算法图解/选择排序/选择排序算法.py
+++ Python相关/算法图解/选择排序/选择排序算法.py
@@ -1,4 +1,4 @@
-def findsmallest (arr):
+def findSmallest (arr):
     smallest = arr[0]
     smallest_index = 0
     for i in range (1, len(arr)):
@@ -6,4 +6,11 @@ def findsmallest (arr):
         smallest_index = i
     return smallest_index
 
-def select
+def selectionSort (arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectionSort([5, 3, 6, 2, 10]))
