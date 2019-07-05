1d50d1720022e87e53b952bc3f8272b8c26084d6
Lee Morales <grandtheftauto710@outlook.com>
2019-07-05 11:22:01 +0800

    终于有输出了，但是输出没对

--- Python相关/算法图解/快速排序/大O表示法实例代码（递归重构）.py
+++ Python相关/算法图解/快速排序/大O表示法实例代码（递归重构）.py
@@ -1,5 +1,5 @@
+total = 0
 def sum(arr):
-    total = 0
     sum =[2, 3, 5, 7, 9]
     if sum <= 0:
         print("Total is 0")
@@ -7,4 +7,4 @@ def sum(arr):
         for x in sum:
             total + x
             return total    
-    print (total)
+print (total)
