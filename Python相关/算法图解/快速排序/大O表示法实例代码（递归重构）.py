5dcf7c202ea0a71810fb74ec2bd427ea2495c4d8
Lee Morales <grandtheftauto710@outlook.com>
2019-07-05 11:17:32 +0800

    还是没有输出

--- Python相关/算法图解/快速排序/大O表示法实例代码（递归重构）.py
+++ Python相关/算法图解/快速排序/大O表示法实例代码（递归重构）.py
@@ -1,10 +1,10 @@
-sum = input("Please imput your array")
 def sum(arr):
-    total = 0
-    if sum = 0:
+    sum =[2, 3, 5, 7, 9]
+    if sum <= 0:
         print("Total is 0")
     else:
-        for x in arr:
-            total += x
-        return total    
-        print (sum)
+        total = 0
+        for x in sum:
+            total + x
+            return total    
+    print (total)
