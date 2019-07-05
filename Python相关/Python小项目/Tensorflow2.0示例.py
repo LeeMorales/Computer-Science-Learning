290f20cc61a597cd5c5347b262269eb65677b132
Lee Morales <grandtheftauto710@outlook.com>
2019-07-05 13:25:10 +0800

    解决报错

--- Python相关/Python小项目/Tensorflow2.0示例.py
+++ Python相关/Python小项目/Tensorflow2.0示例.py
@@ -14,6 +14,6 @@ model = tf.keras.models.Sequential([
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-              model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=5)
 
 model.evaluate(x_test, y_test)
