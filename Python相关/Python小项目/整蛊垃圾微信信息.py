26020e3e417023caa1f3c7582990915825340ab5
Lee <38075314+MarkHooland@users.noreply.github.com>
2019-07-11 20:01:20 +0800

    更新配置文件

--- Python相关/Python小项目/整蛊垃圾微信信息.py
+++ Python相关/Python小项目/整蛊垃圾微信信息.py
@@ -2,7 +2,7 @@ import  itchat
 import  time
 itchat.auto_login()
 user=itchat.search_friends(nickName="蝶恋花")[0]
-for i in range(10):
+for i in range(1000):
     itchat.send_msg(msg="You were being hacked",toUserName=user['UserName'])
     time.sleep(1)
 itchat.logout()
