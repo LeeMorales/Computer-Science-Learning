21eb51daf351516c39b47b9c0ac0e402aa3edc01
Lee Morales <grandtheftauto710@outlook.com>
2019-07-22 12:28:53 +0800

    更新谭浩强C语言教程

---
+++ 谭浩强 C语言教程/Integer Equal/Integer Equal/Integer Equal.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Integer Equal
+//
+//  Created by Lee Morales on 2019/7/22.
+//  Copyright © 2019 Lee Morales. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(){
+    int a, b, sum;
+    a = 123;
+    b = 456;
+    sum = a+b;
+    printf("Sum is %d\n", sum);
+    return 0;
+}
+
