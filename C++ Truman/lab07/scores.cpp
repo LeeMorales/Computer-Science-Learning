8bf310880d60d1009c27b9563801742afe2727e6
Miles Lee <1789216340lhy@gmail.com>
2019-10-02 18:59:02 -0500

    更新运行文件

--- C++ Truman/lab07/scores.cpp
+++ C++ Truman/lab07/scores.cpp
@@ -4,7 +4,6 @@
 //Copyright @ 2019 Hengyi Li. All rights reserved.
 
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 #include <istream>
 
@@ -13,7 +12,7 @@ using namespace std;
 int main()
 {
   ifstream in_file;
-  in_file.open("/Users/leemiles/Desktop/lab07/scores.txt");
+  in_file.open("../scores.txt");
 
   unsigned number_of_scores;
   in_file >> number_of_scores;
@@ -25,39 +24,7 @@ int main()
   string line;
   while (getline(in_file, line))
   {
-    string name_of_user;
-    getline(in_file, name_of_user);
-    for (unsigned loop_times = number_of_scores;
-         loop_times <= number_of_scores;
-         loop_times++)
-    {
-      string numbers;
-      getline(in_file, numbers);
-    }
-    unsigned numbers;
-    double average_score;
-    average_score = (numbers / number_of_scores);
-    double highest_score;
-
-    double lowest_score;
-
-    if (average_score >= 90)
-    {
-      cout << name_of_user << average_score << highest_score << lowest_score
-           << "A" << endl;
-    } else if (average_score >= 80 && average_score < 90)
-    {
-      cout << name_of_user << average_score << highest_score << lowest_score
-           << "B" << endl;
-    } else if (average_score >= 70 && average_score < 80)
-    {
-      cout << name_of_user << average_score << highest_score << lowest_score
-           << "C" << endl;
-    } else
-    {
-      cout << name_of_user << average_score << highest_score << lowest_score
-           << "D" << endl;
-    }
+    cout << line << endl;
     in_file.close();
   }
   return 0;
