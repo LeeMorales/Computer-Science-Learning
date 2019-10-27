4e9c1065418e3889833dcff0154163482441a2af
Miles Lee <1789216340lhy@gmail.com>
2019-10-27 15:40:51 -0500

    更新浮点运算测试结果

--- C++探索/Float_test.cpp
+++ C++探索/Float_test.cpp
@@ -15,7 +15,7 @@ int main()
     float a=3.1415926535898,b=30.1415926535898,c=300.14159265358,d;
 
     for(int i=0;i<T;i++){
-            d=a*b+c;
+            d=a*b*b*a+c*a*b*c;
     }
     printf("%.2f\n",(double)T/((double)clock()/CLOCKS_PER_SEC));
 
