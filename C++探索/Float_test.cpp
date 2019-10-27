5249a785d1f8d0ee72b8333bd34d04df0514c9b6
Miles Lee <1789216340lhy@gmail.com>
2019-10-27 15:38:46 -0500

    更新浮点测试程序

---
+++ C++探索/Float_test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <algorithm>
+#include <cstring>
+
+
+using namespace std;
+
+const int T=1000000000;
+
+int main()
+{
+
+    float a=3.1415926535898,b=30.1415926535898,c=300.14159265358,d;
+
+    for(int i=0;i<T;i++){
+            d=a*b+c;
+    }
+    printf("%.2f\n",(double)T/((double)clock()/CLOCKS_PER_SEC));
+
+    return 0;
+}
