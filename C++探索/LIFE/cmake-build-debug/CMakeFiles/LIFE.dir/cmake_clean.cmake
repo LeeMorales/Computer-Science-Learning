ed9203400c362f177d299f1b7e31ad9e1ddb40f3
Miles Lee <1789216340lhy@gmail.com>
2019-11-11 21:58:51 -0600

    更新life

---
+++ C++探索/LIFE/cmake-build-debug/CMakeFiles/LIFE.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LIFE.dir/main.cpp.o"
+  "LIFE"
+  "LIFE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LIFE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
