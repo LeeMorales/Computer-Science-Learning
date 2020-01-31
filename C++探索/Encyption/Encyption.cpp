daab3f6d79c5e7525c4756b05cc3d6adab1e09e5
Miles Lee <1789216340lhy@gmail.com>
2020-01-31 10:52:04 -0600

    Update

---
+++ C++探索/Encyption/Encyption.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+  string passwd;
+  vector<string> passwd_vector;
+  cout << "Please enter your password: ";
+  getline(cin, passwd);
+
+
+}
