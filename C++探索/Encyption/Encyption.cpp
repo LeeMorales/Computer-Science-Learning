542404486c9c4fd092c5e14677d07d8b07a99db3
Miles Lee <1789216340lhy@gmail.com>
2020-01-31 17:23:34 -0600

    Update

--- C++探索/Encyption/Encyption.cpp
+++ C++探索/Encyption/Encyption.cpp
@@ -4,8 +4,6 @@ using namespace std;
 
 int main()
 {
-  vector<string> alphabet ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  vector<string> alphabet_upper['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   string passwd_plain;
   cout << "Please enter your password: ";
   getline(cin, passwd_plain);
@@ -13,5 +11,5 @@ int main()
   cout << "Please input your key(included negatives): ";
   cin >> values_key;
   
-
+  return 0;
 }
