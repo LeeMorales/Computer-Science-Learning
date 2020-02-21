3825a76a36385d944b48561e68f4471b1762d0a7
Lee <38075314+MarkHooland@users.noreply.github.com>
2020-02-20 23:23:36 -0600

    update

--- C++探索/Application/Test.cpp
+++ C++探索/Application/Test.cpp
@@ -7,5 +7,10 @@ int main(int argc, char *argv[])
   QApplication a(argc, argv);QDialog w;
   w.resize(500, 600);
   QLabel label(&w);
-  
+  label.move(200, 200);
+  label.setTet("Henllo");
+  w.show();
+
+  return a.exec();
+
 }
