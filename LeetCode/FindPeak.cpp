7199971ab32e8f5af73cf0ff836f9faaf7e3432b
Miles Lee <1789216340lhy@gmail.com>
2020-05-06 03:58:50 -0500

    update

--- LeetCode/FindPeak.cpp
+++ LeetCode/FindPeak.cpp
@@ -4,15 +4,22 @@
 
 
 using namespace std;
-int findPeakElement(vector<int>& nums);
+int findPeakElement(vector<int> &nums);
 
 int main() 
 {
-  vector<int> nums = {1,2,1,3,5,6,4};
+  vector<int> nums;
+  nums.push_back(1);
+  nums.push_back(2);
+  nums.push_back(1);
+  nums.push_back(3);
+  nums.push_back(5);
+  nums.push_back(6);
+  nums.push_back(4);
   cout << findPeakElement(nums);
   return 0;
 }
-int findPeakElement(vector<int>& nums) 
+int findPeakElement(vector<int> &nums) 
 {
   if(nums.size() == 0)
   {
@@ -22,11 +29,13 @@ int findPeakElement(vector<int>& nums)
   int left = 0;
   int right = nums.size();
   int mid;
-  while(left < right)
+  bool done = false;
+  while(!done)
   {
     mid = left + (right - left) / 2;
     if(mid > left && mid > right)
     {
+      done = true;
       return mid;
     }
     else if(mid > left && mid < right)
