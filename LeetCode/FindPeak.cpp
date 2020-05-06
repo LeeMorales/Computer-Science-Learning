a1173483e56b90b88d1c69113638b7c6d2dbc30f
Miles Lee <1789216340lhy@gmail.com>
2020-05-06 01:52:30 -0500

    update

--- LeetCode/FindPeak.cpp
+++ LeetCode/FindPeak.cpp
@@ -1,87 +1,42 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <cstdlib>
-#include <cstring>
 #include <algorithm>
 
 
 using namespace std;
-
-class Solution 
-{
-public:
-    int findPeakElement(vector<int>& nums) 
-    {
-       if(nums.size() == 0)
-       {
-          return -1;
-       }
-
-        int left = 0;
-        int right = nums.size();
-        int mid;
-        while(left < right)
-        {
-          mid = left + (right - left) / 2;
-          if(mid > left && mid > right)
-          {
-            return mid;
-          }
-          else if(mid > left && mid < right)
-          {
-            mid = right - 1;
-          }
-          else
-          {
-            mid = left + 1;
-          }
-        }
-      return nums.at(mid);
-    }
-};
-void trimLeftTrailingSpaces(string &input);
-void trimRightTrailingSpaces(string &input);
-vector<int> stringToIntegerVector(string input);
-
+int findPeakElement(vector<int>& nums);
 
 int main() 
 {
-    string line;
-    while (getline(cin, line)) 
+  vector<int> nums = {1,2,1,3,5,6,4};
+  cout << findPeakElement(nums);
+  return 0;
+}
+int findPeakElement(vector<int>& nums) 
+{
+  if(nums.size() == 0)
+  {
+    return -1;
+  }
+
+  int left = 0;
+  int right = nums.size();
+  int mid;
+  while(left < right)
+  {
+    mid = left + (right - left) / 2;
+    if(mid > left && mid > right)
     {
-        vector<int> nums = stringToIntegerVector(line);
-        
-        int ret = Solution().findPeakElement(nums);
-
-        string out = to_string(ret);
-        cout << out << endl;
+      return mid;
     }
-    return 0;
-}
-void trimLeftTrailingSpaces(string &input) 
-{
-    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) { return !isspace(ch);}));
-}
-
-void trimRightTrailingSpaces(string &input) 
-{
-    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {return !isspace(ch);}).base(), input.end());
-}
-
-vector<int> stringToIntegerVector(string input) 
-{
-    vector<int> output;
-    trimLeftTrailingSpaces(input);
-    trimRightTrailingSpaces(input);
-    input = input.substr(1, input.length() - 2);
-    stringstream ss;
-    ss.str(input);
-    string item;
-    char delim = ',';
-    while (getline(ss, item, delim)) 
+    else if(mid > left && mid < right)
     {
-        output.push_back(stoi(item));
+      mid = right - 1;
     }
-    return output;
+    else
+    {
+      mid = left + 1;
+    }
+  }
+return nums.at(mid);
 }
