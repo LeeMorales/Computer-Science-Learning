a35db300f0d41118b0a3dd6d0d971c63aecc3aa8
TheRealMilesLee <1789216340lhy@gmail.com>
2020-07-05 11:15:48 +0800

    update

--- C++_Truman/CS180-Fall2019/Muti-thread/lab03b/Java_Joe.cpp
+++ C++_Truman/CS180-Fall2019/Muti-thread/lab03b/Java_Joe.cpp
@@ -12,6 +12,17 @@
 using namespace std;
 
 /**
+ * This function is to get the user input
+ * @param name is the name of the user. 
+ * @param colombian_quantity is the total amount of the colombian
+ * @param deca_quantity is the total amount of the deca
+ * @param chai_quantity is the total amount of the chai
+ */
+void get_user_input(string name, unsigned colombian_quantity, unsigned deca_quantity, unsigned chai_quantity);
+
+double calculation(unsigned colombian_quantity, unsigned deca_quantity, unsigned chai_quantity);
+
+/**
  * This function is to get the numer in random
  * @param MAX_VALUE is the max limit of the random number
  * @param MIN_VALUE is the min limit of the random number
@@ -21,9 +32,7 @@ double get_random_bonus(const unsigned MAX_VALUE, const unsigned MIN_VALUE);
 
 int main() 
 {
-  const double UNIT_PRICE_COLOMBIAN= 0.35;
-  const double UNIT_PRICE_DECAF = 0.85;
-  const double UNIT_PRICE_CHAI= 1.10;
+
   const unsigned LABEL_WIDTH_COLOM = 14;
   const unsigned LABEL_WIDTH_ORGAN = 12;
   const unsigned LABEL_WIDTH_HEAVEN = 12;
@@ -33,20 +42,11 @@ int main()
   unsigned colombian_quantity;
   unsigned deca_quantity;
   unsigned chai_quantity;
-
-  cout << "Welcome to Java Joe's!" << endl;
-  cout << "Enter your name: ";
   string name;
-  getline (cin,name);
 
-  cout << "How many ounces of Colombian, Decaf, and Chai?: ";
-  cin >> colombian_quantity >> deca_quantity >> chai_quantity;
-  cout << endl << "Invoice for Fred Flintstone" << endl;
+  get_user_input(name,colombian_quantity,deca_quantity,chai_quantity);
 
-  double amount_colom_supr =  colombian_quantity * UNIT_PRICE_COLOMBIAN;
-  double amount_organ_gold =  deca_quantity * UNIT_PRICE_DECAF;
-  double amount_heavn_chai =  chai_quantity * UNIT_PRICE_CHAI;
-  double total =  amount_colom_supr + amount_organ_gold + amount_heavn_chai;
+  calculation(colombian_quantity,deca_quantity,chai_quantity);
 
   cout << "                  Colom Supr  Organ Gold  Heavn Chai" << endl;
   cout << "                  ----------  ----------  ----------" << endl;
@@ -88,4 +88,28 @@ double get_random_bonus(const unsigned MAX_VALUE, const unsigned MIN_VALUE)
      double bonus_discount = static_cast<double>(rand() % (MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE);
 
      return bonus_discount;
+}
+
+void get_user_input(string name, unsigned colombian_quantity, unsigned deca_quantity, unsigned chai_quantity)
+{
+   cout << "Welcome to Java Joe's!" << endl;
+   cout << "Enter your name: ";
+   getline (cin,name);
+   cout << "How many ounces of Colombian, Decaf, and Chai?: ";
+   cin >> colombian_quantity >> deca_quantity >> chai_quantity;
+   cout << endl << "Invoice for Fred Flintstone" << endl;    
+}
+
+double calculation(unsigned colombian_quantity, unsigned deca_quantity, unsigned chai_quantity)
+{ 
+  const double UNIT_PRICE_COLOMBIAN= 0.35;
+  const double UNIT_PRICE_DECAF = 0.85;
+  const double UNIT_PRICE_CHAI= 1.10;
+  
+  double amount_colom_supr =  colombian_quantity * UNIT_PRICE_COLOMBIAN;
+  double amount_organ_gold =  deca_quantity * UNIT_PRICE_DECAF;
+  double amount_heavn_chai =  chai_quantity * UNIT_PRICE_CHAI;
+  double total =  amount_colom_supr + amount_organ_gold + amount_heavn_chai;
+
+  return amount_colom_supr, amount_organ_gold, amount_heavn_chai;
 }
